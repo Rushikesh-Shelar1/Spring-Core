a1d2b2a3ae9573d2a52c08752b5bd57927fb131d
Rushikesh-Shelar1 <167902536+Rushikesh-Shelar1@users.noreply.github.com>
2024-09-18 20:23:59 +0530

    Spring core

---
+++ src/main/java/com/springcore/constuctor/Certi.java
@@ -0,0 +1,20 @@
+package com.springcore.constuctor;
+
+public class Certi {
+
+	String name;
+
+	/**
+	 * @param name
+	 */
+	public Certi(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+}
